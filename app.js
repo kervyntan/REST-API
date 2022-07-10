9bd3719383619710ff2e9866c06f99b57df7df41
Kervyn <kervyntan@gmail.com>
2022-07-10 22:20:16 +0800

    added dummy endpoint for API

--- app.js
+++ app.js
@@ -3,21 +3,46 @@ const express = require('express');
 // express app set up
 const app = express(); // creates instance of express app
 
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'folderName') <-- if folder name is not views
+
 // listen for requests
 app.listen(3000);
 
+// middleware logging
+app.use((req, res, next) => {
+    console.log('new request made');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next(); // fires this function to tell Express to carry on in the code (goes to next middleware)
+})
+
+// dummy API endpoint
+app.get('/info', (req, res) => {
+    res.sendFile('./data/data.json', {root : __dirname})
+})
 // respond to the requests
 // first arg -> the path you listen to
 // second arg -> callback function
 app.get('/', (req, res) => {
         // need to specifiy the root directory (__dirname to get current dir name)
-    res.sendFile('./views/index.html', { root : __dirname }); 
+    // res.sendFile('./views/index.html', { root : __dirname }); 
+
+    const blogs = [
+        {title : "This is title 1", snippet : 'Trolling 1'},
+        {title : "This is title 2", snippet : 'Trolling 2'},
+        {title : "This is title 3", snippet : 'Trolling 3'}
+    ]
+    res.render('index', {title : 'flying', blogs});
 })
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root : __dirname });
+    // res.sendFile('./views/about.html', { root : __dirname });
+    res.render('about', { title : 'about'});
 })
 app.get('/about-us', (req, res) => {
-    res.redirect('/about'); // redirect to this path instead 
+    res.redirect('/about', { title : '404'}); // redirect to this path instead 
 })
 
 // 404 page
@@ -28,4 +53,5 @@ app.get('/about-us', (req, res) => {
 // after executing a redirect, node will stop reviewing the remaining code after that line
 app.use((res, req) => {
     res.sendFile('./views/404.html', { root : __dirname })
+    // res.render('404', { title : '404' })
 })
